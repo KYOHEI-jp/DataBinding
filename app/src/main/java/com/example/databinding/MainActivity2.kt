5ba2f4bcb841c25002289fcf46c90eaec590a120
渡辺恭平 <kingdomhearts510@gmail.com>
2021-04-03 12:29:26 +0900

    databinding使用例

--- app/src/main/java/com/example/databinding/MainActivity2.kt
+++ app/src/main/java/com/example/databinding/MainActivity2.kt
@@ -2,12 +2,16 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
 
+    lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+        binding.textViewId.text = "Shiva2"
     }
-
 }
