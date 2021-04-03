2d471c611543a5425395580d5c9a2b78a4652e15
渡辺恭平 <kingdomhearts510@gmail.com>
2021-04-03 15:30:19 +0900

    ViewModelのtextDataを表示出来た。

--- app/src/main/java/com/example/databinding/MainActivity2.kt
+++ app/src/main/java/com/example/databinding/MainActivity2.kt
@@ -3,17 +3,21 @@ package com.example.databinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.databinding.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
     //MainActivity2と紐づける
     lateinit var binding: ActivityMain2Binding
+    lateinit var viewModel: ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //bindingにMainActivity2をセットする
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
-        //MainActivityのidついているウィジェットを使用出来るようになる。
-        binding.textViewId.text = "Shiva2"
+        //viewModelクラスのインスタンスを作成
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+
+        binding.viewModel = viewModel
     }
 }
---
+++ app/src/main/java/com/example/databinding/ViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.databinding
+
+import androidx.lifecycle.ViewModel
+
+class ViewModel: ViewModel() {
+
+    var textData = "TEST"
+
+}
