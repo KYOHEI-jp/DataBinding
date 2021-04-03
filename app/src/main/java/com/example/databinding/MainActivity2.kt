3440429dd2fcb91bff1d1219902eb7460458d0a8
渡辺恭平 <kingdomhearts510@gmail.com>
2021-04-03 14:53:51 +0900

    comment追記

--- app/src/main/java/com/example/databinding/MainActivity2.kt
+++ app/src/main/java/com/example/databinding/MainActivity2.kt
@@ -6,12 +6,14 @@ import androidx.databinding.DataBindingUtil
 import com.example.databinding.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
-
+    //MainActivity2と紐づける
     lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //bindingにMainActivity2をセットする
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+        //MainActivityのidついているウィジェットを使用出来るようになる。
         binding.textViewId.text = "Shiva2"
     }
 }
